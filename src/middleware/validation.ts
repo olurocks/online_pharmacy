7b5d4f428232ba4df2b64a466bb7ac27e2b55210
olur0cks <olurocks1@gmail.com>
2025-08-21 09:44:45 +0100

    Implement Validation middleware

---
+++ src/middleware/validation.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import Joi from "joi";
+import { AppError } from "./errorHandler.ts";
+
+export const validate = (
+  schema: Joi.ObjectSchema,
+  source: "body" | "query" | "params" = "body"
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const data = req[source];
+    const { error, value } = schema.validate(data, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    if (error) {
+      const details = error.details.map((detail) => ({
+        field: detail.path.join("."),
+        message: detail.message,
+        value: detail.context?.value,
+      }));
+
+      return next(new AppError("Validation Error", 400));
+    }
+
+    req[source] = value;
+    next();
+  };
+};

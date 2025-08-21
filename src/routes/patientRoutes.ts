c076e54e29ecae7d249a8ef4fba24ab1195ecd0d
olur0cks <olurocks1@gmail.com>
2025-08-21 09:26:01 +0100

    Patient Routes

---
+++ src/routes/patientRoutes.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import {
+  createPatient,
+  getPatients,
+  getPatientById,
+  updatePatient,
+  searchPatients,
+  deletePatient,
+} from "../controllers/patientController"; //to do
+import { validate } from "../middleware/validation"; //to do
+import {
+  createPatientSchema,
+  updatePatientSchema,
+  searchPatientSchema,
+  uuidSchema,
+  paginationSchema,
+} from "../utils/validation"; //to do
+
+const router = Router();
+
+router.post("/", validate(createPatientSchema), createPatient);
+
+router.get("/", validate(paginationSchema, "query"), getPatients);
+
+router.get("/search", validate(searchPatientSchema, "query"), searchPatients);
+
+router.get("/:id", validate(uuidSchema, "params"), getPatientById);
+
+router.put(
+  "/:id",
+  [validate(uuidSchema, "params"), validate(updatePatientSchema)],
+  updatePatient
+);
+
+router.delete("/:id", validate(uuidSchema, "params"), deletePatient);
+
+export default router;
